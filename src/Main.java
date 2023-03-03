cb74311e32129e4244045023beb8935652f60590
ASIF <imdasif9@gmail.com>
2023-03-03 20:42:08 +0530

    Added the calculator program

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /*System.out.println("Hello world!");
+        System.out.println("Program begins");
+        int x = 123;
+        System.out.println("Value x=");
+        System.out.println(x);
+        x++;
+        System.out.println("Now, value x=");
+        System.out.println(x);
+        System.out.println("Program terminates");
+    */
+        int a =1 ;
+        int b = 2;
+        float ab = 1;
+        float cd = 2;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        float divi = ab / cd;
+        int mod = a % b;
+        System.out.println("Addition = "+sum);
+        System.out.println("Subtraction = "+sub);
+        System.out.println("Multiplication = "+mul);
+        System.out.println("Division of int number = "+div);
+        System.out.println("Division of float number = "+divi);
+        System.out.println("Modulous is = "+mod);
+        System.out.println("Quotient = "+div);
+        System.out.println("Remainder is = "+mod);
+    }
+}
